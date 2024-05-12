477840b599b3dbbfb45999528c7ab41dc8177413
seungju.kim <pipiru100@gmail.com>
2024-05-12 15:24:13 +0900

    add

--- src/main/java/com/fc/testcicd/controller/TestController.java
+++ src/main/java/com/fc/testcicd/controller/TestController.java
@@ -5,9 +5,11 @@ import com.fc.testcicd.service.TestServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
     private TestService testService;
